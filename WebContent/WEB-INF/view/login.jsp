1233009c14f1bfe15277b722fc21d83231dd24ee
thuctonhuyen <thuctonhuyen@gmail.com>
2017-04-23 01:57:29 -0700

    log-in processing

--- WebContent/WEB-INF/view/login.jsp
+++ WebContent/WEB-INF/view/login.jsp
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-
-
 <%@ include file="header.jsp"%>
 
 <body>
@@ -13,6 +11,8 @@
 			<div class="col-lg-12">
 				<div class="login-container">
 					<h3>Login to your account</h3>
+					
+					<p align="center" style="color:red;"> ${message} </p>
 					<form id="login-form" action="processLoginForm" method="post" role="form">
 						<input type="text" name="email" placeholder="Email" required>
 						<input type="password" name="password" placeholder="Password"
--- src/com/spring/controller/CustomerController.java
+++ src/com/spring/controller/CustomerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.spring.dao.CustomerDao;
 import com.spring.model.Movie;
@@ -22,20 +24,40 @@ public class CustomerController {
 	CustomerDao customerdao;
 	
 	@RequestMapping(value="/processLoginForm",method=RequestMethod.POST)
-	public String processLoginForm(HttpServletRequest request, Model model){
+	public ModelAndView processLoginForm(HttpServletRequest request, RedirectAttributes redir){
 		Boolean isCustomer = customerdao.isCustomer(request.getParameter("email")
 				, request.getParameter("password"));
 		
 		if(isCustomer)
 		{
-			return "index";
+			//todo: change session 
+			HttpSession session = request.getSession(true);
+			Integer login = (Integer) (session.getAttribute("login"));
+			if(login == null)
+			{
+				login = new Integer(1);
+				session.setAttribute("login", login);
+			}
+			
+			return new ModelAndView("redirect:/index"); 
 		}
 		
 		else
 		{
-			model.addAttribute("message", "Invalid Email/Password Combination");
 			
-			return "login"; 
+			//todo: change session 
+			HttpSession session = request.getSession(true);
+			Integer login = (Integer) (session.getAttribute("login"));
+			if(login == null)
+			{
+				login = new Integer(0);
+				session.setAttribute("login", login);
+			}
+			
+			ModelAndView model = new ModelAndView();
+			model.setViewName("redirect:/login");
+			redir.addFlashAttribute("message", "Invalid Email/Password combination. Please try again!");
+			return model; 
 		}
 		
 	}
