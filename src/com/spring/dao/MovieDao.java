b36b7da589c4664f6f44b758cd6b5f5624bfc4c8
thuctonhuyen <thuctonhuyen@gmail.com>
2017-04-22 23:25:14 -0700

    resolve conflict

--- src/com/spring/dao/MovieDao.java
+++ src/com/spring/dao/MovieDao.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.spring.dao;
 
 import java.util.List;
@@ -13,19 +13,4 @@ public interface MovieDao {
 	public List<Movie> getMovieListWhereTitlesStartWith(String StartWith);
 	
 }
-=======
-package com.spring.dao;
 
-import java.util.List;
-
-
-import com.spring.model.Movie;;
-
-public interface MovieDao {
-	
-	public List<Movie> getMovieList();
-	
-	public List<Movie> getMovieListWhereTitlesStartWith(String StartWith);
-	
-}
->>>>>>> c8faefbdb09912298e1b348cb9ea9ed0dd863d43
