d49eeee7b9ce31e3c8889a561ed65166147998ed
thuctonhuyen <thuctonhuyen@gmail.com>
2017-05-28 20:21:44 -0700

    minor fix

--- WebContent/resources/js/custom.js
+++ WebContent/resources/js/custom.js
@@ -85,7 +85,7 @@ var hooverMovieDetails = function(){
             if (!$(".popover:hover").length) {
                 $(_this).popover("hide");
             }
-        }, 30);
+        }, 20);
 });
 	
 	function getMovie(){
